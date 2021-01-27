036106a9a11a4522f0bcea66358a30a5c513c471
ikeda_yuichiro_110 <ikeda.yuichiro.w.00@gmail.com>
2021-01-27 22:54:13 +0900

    ゲーム画面その他

--- Assets/#developData/#ikeda/AirPlaneCtrl.cs
+++ Assets/#developData/#ikeda/AirPlaneCtrl.cs
@@ -16,6 +16,8 @@ public class AirPlaneCtrl : MonoBehaviour
     public float c;
 
 
+    [Range(-1, 1)]
+    public float d;
     void Start()
     {
         rigidbody_ = GetComponent<Rigidbody>();
@@ -24,13 +26,11 @@ public class AirPlaneCtrl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        if (Input.GetKey(KeyCode.T)) Buoyancy += Time.deltaTime * b;
-        if (Input.GetKey(KeyCode.G)) Buoyancy -= Time.deltaTime * b;
-
+        var input = Key.JoyStickL.Get;
+        Buoyancy += Time.deltaTime * Key.JoyStickR.Get.y;
+       
         Accel += input.y * Time.deltaTime *a;
-        if (-0.03 < Accel && Accel < 0.03)
-            Accel = 0;
+        if (-0.03 < Accel && Accel < 0.03) Accel = 0;
         if (Accel < -1000) Accel = -1000;
         if (Accel > 3000) Accel = 3000;
         Accel *= 0.97f;
@@ -50,7 +50,7 @@ public class AirPlaneCtrl : MonoBehaviour
         ///  float xxx = -transform.rotation.eulerAngles.x; 
         /// Buoyancy += Time.deltaTime * (Mathf.Abs(xxx) > 45 ? xxx%45 : xxx); 
         rigidbody_.AddRelativeForce(new Vector3(0, 0, 1)* Accel * Time.deltaTime, ForceMode.Acceleration);
-        rigidbody_.AddRelativeTorque(-Vector3.Scale(transform.rotation.ToEuler(), new Vector3(1, 0, 1)) * Accel * Time.deltaTime, ForceMode.Acceleration); //
+        rigidbody_.AddRelativeTorque(d * -Vector3.Scale(transform.rotation.ToEuler(), new Vector3(1, 0, 1)) * Accel * Time.deltaTime, ForceMode.Acceleration); //
 
     }
 }
